c49cbd6344ec6f01ffec694f58e82fb3d711e514
Bettina Junghahn <bettinajunghahn@gmail.com>
2018-05-01 16:21:58 -0400

    react-nyt-app on Heroku

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Jumbotron.css";
+
+const Jumbotron = () =>
+
+  <div className="jumbotron jumbotron-fluid">
+    <div className="container">
+      <span className="float-right"><strong><a href="/">Search</a> | <a href="/saved">Saved</a></strong></span>
+    </div>
+  </div>
+
+export default Jumbotron;
